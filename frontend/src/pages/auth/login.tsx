6b22558d49d1880b47b528f57059728f9afd6e0d
ashraf <sadarsign@gmail.com>
2025-03-06 15:25:18 +0300

    Initialized

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,7 +1,7 @@
 
 const Login = () => {
     return (
-        <div>
+        <div className="w-[80%] border p-3 rounded-md shadow-sm md:w-[60%] lg:w-[40%] mx-auto">
             <div className="header">
                 <h1 className="text-xl font-bold">
                     Sing in
@@ -10,6 +10,21 @@ const Login = () => {
                     please enter your credentials to access your account
                 </p>
             </div>
+            <form>
+                <div className="inputContainer grid my-3">
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" className="border p-3 rounded-md" placeholder="Enter your email" />
+                </div>
+                <div className="inputContainer grid my-3">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="border p-3 rounded-md" placeholder="Enter your password" />
+                </div>
+                <div className="grid items-center px-40">
+                    <button className="p-3 rounded-md my-3 bg-gray-800 hover:bg-gray-700 text-white">
+                        Sing in
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
