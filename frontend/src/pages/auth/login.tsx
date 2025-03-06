a79bdc2f0bf199c3810f5daa21a02122620aefc3
ashraf <sadarsign@gmail.com>
2025-03-06 15:12:42 +0300

    Initialized

---
+++ frontend/src/pages/auth/login.tsx
@@ -0,0 +1,17 @@
+
+const Login = () => {
+    return (
+        <div>
+            <div className="header">
+                <h1 className="text-xl font-bold">
+                    Sing in
+                </h1>
+                <p className="text-sm text-gray-500">
+                    please enter your credentials to access your account
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainRouter from "./pages/main";
 import Homepage from "./pages/Homepage";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/auth/login";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,15 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <Homepage />
+            },
+            {
+                path: "auth",
+                children: [
+                    {
+                        path: "login",
+                        element: <Login />
+                    }
+                ]
             },
             {
                 path: "*",
