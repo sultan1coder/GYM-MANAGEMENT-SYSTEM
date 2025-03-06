9b9d5c3687316c6a729fcc04bcd8b7e6aa9e9dd3
ashraf <sadarsign@gmail.com>
2025-03-06 14:55:01 +0300

    Initialized

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,16 +3,21 @@ import notFoundImage from "../assets/notFoundImage.png";
 
 const NotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-            <img src={notFoundImage} alt="Not Found" className="mx-auto mb-6 w-48 h-auto" />
-            <h1 className="text-4xl font-bold text-gray-800 mt-6">Page Not Found</h1>
-            <p className="text-gray-600 mt-2">Oops! The page you are looking for does not exist.</p>
-            <Link
-                to="/"
-                className="mt-4 px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition"
-            >
-                Go Home
-            </Link>
+        <div className="flex items-center justify-center">
+            <div className="text-center bg-white p-8 w-full max-w-lg">
+                <img
+                 src={notFoundImage}
+                 alt="Not Found" 
+                 className="mx-auto mb-6 w-72 h-auto" />
+                <h1 className="text-4xl font-semibold text-gray-800 mb-4">404 Not Found</h1>
+                <p className="text-gray-600 text-lg mb-6">Oops! The page you are looking for does not exist.</p>
+                <Link
+                    to="/"
+                    className=" px-6 py-3 bg-gray-800 text-white text-lg font-medium rounded-lg shadow-md hover:bg-gray-700 transition"
+                >
+                    Go Home
+                </Link>
+            </div>
         </div>
     );
 }
