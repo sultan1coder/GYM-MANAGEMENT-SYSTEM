c084d35041d3aad9a40022595f6d39ec440e9d0f
ashraf <sadarsign@gmail.com>
2025-03-06 14:27:10 +0300

    Initialized

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex items-center justify-around p-3'>
             <div className="logo">
                 <h1>BILKHAYR GYM</h1>
             </div>
-            <div className="menus">
-                <Link to={"/login"}>Login</Link>
-                <Link to={"/register"}>Register</Link>
+            <div className="menus flex gap-4">
+                <Link className='bg-gray-800 text-white p-3 rounded-md hover:bg-gray-700 transition-all' to={"/login"}>Login</Link>
+                <Link className='bg-gray-800 text-white p-3 rounded-md hover:bg-gray-700 transition-all' to={"/register"}>Register</Link>
             </div>
         </div>
     )
