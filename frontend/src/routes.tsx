b9708788c8a1f6bfd63fc7c92d5f434494f37e6e
ashraf <sadarsign@gmail.com>
2025-03-06 14:10:49 +0300

    Initialized

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,3 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainRouter from "./pages/main";
+import Homepage from "./pages/Homepage";
 
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainRouter />,
+        children: [
+            {
+                index: true,
+                element: <Homepage />
+            }
+        ]
+    }
+]);
