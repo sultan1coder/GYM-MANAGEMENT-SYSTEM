1e86347bcfa5541acfe424e141bd2f2a51adc3f6
ashraf <sadarsign@gmail.com>
2025-03-07 21:56:20 +0300

    Initialized

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -2,17 +2,34 @@
 const Register = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-lg p-8 bg-gray-300 shadow-lg rounded-xl">
+            <div className=" max-w-lg p-8 bg-gray-300 shadow-lg min-h-[500px] rounded-xl">
                 <h2 className="mb-6 text-2xl font-semibold text-center">Registration</h2>
                 <form className="space-y-4 ">
                     <div className="grid grid-cols-2 gap-4">
-                        {/* <label htmlFor="text">Fullname</label> */}
-                        <input type="text" name="fullName" placeholder="Full Name" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
-                        <input type="text" name="username" placeholder="Username" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
-                        <input type="email" name="email" placeholder="Email" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
-                        <input type="tel" name="phone" placeholder="Phone Number" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
-                        <input type="password" name="password" placeholder="Password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
-                        <input type="password" name="confirmPassword" placeholder="Confirm Password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        <div>
+                            <label htmlFor="text">Fullname</label>
+                            <input type="text" name="fullName" placeholder="Full Name" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
+                        <div>
+                            <label htmlFor="">Username</label>
+                            <input type="text" name="username" placeholder="Username" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" placeholder="Email" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
+                        <div>
+                            <label htmlFor="phone">Phone</label>
+                            <input type="tel" name="phone" placeholder="Phone Number" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="Password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Confirm Password</label>
+                            <input type="password" name="confirmPassword" placeholder="Confirm Password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                        </div>
                     </div>
 
                     <div>
