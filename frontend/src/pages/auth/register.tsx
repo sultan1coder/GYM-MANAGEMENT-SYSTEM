699a5cd4d58db033bbebc4150a2fab76b8a72516
ashraf <sadarsign@gmail.com>
2025-03-08 12:02:39 +0300

    Initialized

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -8,23 +8,23 @@ const Register = () => {
                     <div className="grid grid-cols-2 gap-4">
                         <div>
                             <label htmlFor="text">Fullname</label>
-                            <input type="text" name="fullName" placeholder="Full Name" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                            <input type="text" name="fullName" placeholder="Enter your name" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
                         </div>
                         <div>
                             <label htmlFor="">Username</label>
-                            <input type="text" name="username" placeholder="Username" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                            <input type="text" name="username" placeholder="Enter your username" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
                         </div>
                         <div>
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="email" placeholder="Email" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                            <input type="email" name="email" placeholder="Enter your email" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
                         </div>
                         <div>
                             <label htmlFor="phone">Phone</label>
-                            <input type="tel" name="phone" placeholder="Phone Number" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                            <input type="tel" name="phone" placeholder="Enter your number" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
                         </div>
                         <div>
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="Password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
+                            <input type="password" name="password" placeholder="Enter your password" className="p-2 mt-1 border border-gray-300 rounded-md input focus:border-blue-500 focus:outline-none" required />
                         </div>
                         <div>
                             <label htmlFor="password">Confirm Password</label>
@@ -46,7 +46,7 @@ const Register = () => {
                         </div>
                     </div>
 
-                    <button type="submit" className="w-full py-2 font-semibold text-white transition rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:opacity-90">
+                    <button type="submit" className="w-full py-2 font-semibold text-white transition bg-gray-800 rounded-lg hover:opacity-90">
                         Register
                     </button>
                 </form>
