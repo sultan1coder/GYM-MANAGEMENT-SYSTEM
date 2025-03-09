5901f3d67bb772f5cee953fee02b93246cdcee52
ashraf <sadarsign@gmail.com>
2025-03-09 22:26:35 +0300

    Initialized

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -107,51 +107,6 @@ const Login = () => {
             </div>
         </div>
     );
-
-    // return (
-    //     <div className="w-[80%] border p-3 rounded-md shadow-sm md:w-[60%] lg:w-[40%] mx-auto mb-10 bg-gray-300">
-    //         <div className="header">
-    //             <h1 className="mb-3 text-2xl font-bold text-center">
-    //                 Sign in
-    //             </h1>
-    //             <p className="text-sm text-center text-gray-600">
-    //                 Please enter your credentials to access your account
-    //             </p>
-    //         </div>
-    //         <form onSubmit={formik.handleSubmit}>
-    //             <div className="grid my-3 inputContainer">
-    //                 <label htmlFor="email">Email Address</label>
-    //                 <input
-    //                     onBlur={formik.handleBlur}
-    //                     name="email"
-    //                     onChange={formik.handleChange}
-    //                     value={formik.values.email}
-    //                     type="email" className="p-3 border rounded-md" placeholder="Enter your email" />
-    //                 {/* Error message */}
-    //                 <p className="text-sm font-semibold text-red-500">
-    //                     {formik.touched.email && formik.errors.email}
-    //                 </p>
-    //             </div>
-    //             <div className="grid my-3 inputContainer">
-    //                 <label htmlFor="password">Password</label>
-    //                 <input
-    //                     onBlur={formik.handleBlur}
-    //                     name="password"
-    //                     onChange={formik.handleChange}
-    //                     value={formik.values.password}
-    //                     type="password" className="p-3 border rounded-md" placeholder="Enter your password" />
-    //                 <p className="text-sm font-semibold text-red-500">
-    //                     {formik.touched.password && formik.errors.password}
-    //                 </p>
-    //             </div>
-    //             <div className="grid items-center px-40">
-    //                 <button className="p-3 my-3 text-white bg-gray-800 rounded-md hover:bg-gray-700">
-    //                     Sing in
-    //                 </button>
-    //             </div>
-    //         </form>
-    //     </div>
-    // )
 }
 
 export default Login
