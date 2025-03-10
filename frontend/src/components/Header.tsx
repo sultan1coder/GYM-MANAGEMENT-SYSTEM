f2aa17e83b9a76e9026a415d6da3a1f60501e3ea
ashraf <sadarsign@gmail.com>
2025-03-10 22:02:31 +0300

    Initialized

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,21 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { RootState } from '../redux/store'
 
 const Header = () => {
+
+    const loginState = useSelector((state: RootState) => state.loginSlice)
+
     return (
         <div className='flex items-center justify-around p-3'>
             <div className="text-xl font-bold logo ">
                 <h1>BILKHAYR GYM</h1>
             </div>
-            <div className="flex gap-4 menus">
+            {loginState.data.isSuccess ? "Logout" : <div className="flex gap-4 menus">
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/"}>HomePage</Link>
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/login"}>Login</Link>
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/register"}>Register</Link>
-
-            </div>
+            </div>}
         </div>
     )
 }
