ce8391a28561c8e0d911191721e3a8b0f83a416e
ashraf <sadarsign@gmail.com>
2025-03-10 23:56:46 +0300

    Initialized

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,23 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { RootState } from '../redux/store'
+import { AppDispatch, RootState } from '../redux/store'
+import { logout } from '../redux/slices/auth/loginSlice'
 
 const Header = () => {
 
     const loginState = useSelector((state: RootState) => state.loginSlice)
+    const dispatch = useDispatch<AppDispatch>()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
     return (
         <div className='flex items-center justify-around p-3'>
             <div className="text-xl font-bold logo ">
                 <h1>BILKHAYR GYM</h1>
             </div>
-            {loginState.data.isSuccess ? <button className='p-3 bg-red-500 rounded-md'>Logout</button> : <div className="flex gap-4 menus">
+            {loginState.data.isSuccess ? <button onClick={logoutHandler} className='p-3 text-white bg-red-500 rounded-md'>Logout</button> : <div className="flex gap-4 menus">
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/"}>HomePage</Link>
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/login"}>Login</Link>
                 <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/register"}>Register</Link>
