e196afe02384e4157484a0b8eccc01b830bf08f1
ashraf <sadarsign@gmail.com>
2025-03-11 12:34:52 +0300

    Initialized

--- frontend/src/components/profile.tsx
+++ frontend/src/components/profile.tsx
@@ -17,7 +17,32 @@ const Profile = () => {
                     </h1>
                 </div>
             </PopoverTrigger>
-            <PopoverContent>Place content for the popover here.</PopoverContent>
+            <PopoverContent>
+                <div className="flex items-center gap-4">
+                    <div className="border w-[42px] h-[42px] flex items-center justify-center rounded-full">
+                        <h1 className="text-2xl font-bold">
+                            {loginState.data.user.name[0].toUpperCase()}
+                        </h1>
+                    </div>
+                    <div className="gap-6">
+                        <h1 className="text-xl font-bold">
+                            {loginState.data.user.name}
+                        </h1>
+                        <p className="text-sm text-gray-600">
+                            {loginState.data.user.email}
+                        </p>
+                        <p className="text-sm text-gray-600">
+                            {loginState.data.user.phone_number}
+                        </p>
+                        <p className="text-sm text-gray-600">
+                            {loginState.data.user.username}
+                        </p>
+                        <p className="text-sm text-gray-600">
+                            {loginState.data.user.id}
+                        </p>
+                    </div>
+                </div>
+            </PopoverContent>
         </Popover>
 
     )
