fd5e4f07d97098df8329ed94effe62651cec038c
ashraf <sadarsign@gmail.com>
2025-03-25 14:53:13 +0300

    Initialized

--- frontend/src/types/member.ts
+++ frontend/src/types/member.ts
@@ -3,6 +3,22 @@ export interface IFetchMembers {
   members: any[];
 }
 
+export interface IGetSingleMember {
+  isSuccess: boolean;
+  message: string;
+  member: Member;
+}
+
+export interface Member {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+  membershiptype: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export interface ICreateResponse {
   isSuccess: boolean;
   message: string;
