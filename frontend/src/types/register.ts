346ca61fabd1645eae39822b5f704961095a5c1c
ashraf <sadarsign@gmail.com>
2025-03-27 23:03:12 +0300

    Initialized

--- frontend/src/types/register.ts
+++ frontend/src/types/register.ts
@@ -1,28 +1,27 @@
 export interface IRegisterResponse {
-    isSuccess: boolean;
-    message: string;
-    newUser: NewUser;
+  isSuccess: boolean;
+  message: string;
+  newUser: NewUser;
 }
 
 export interface NewUser {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    phone_number: null;
-    password: string;
-    confirmPassword: string;
-    role: string;
-    created_at: Date;
-    updated_at: Date;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone_number: null;
+  password: string;
+  confirmPassword: string;
+  role: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
-
 export interface IRegisterBody {
-    name: string;
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    role: string;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: string;
 }
