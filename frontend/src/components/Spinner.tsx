a04dc8e697346737f8a5a728f21183453188439e
ashraf <sadarsign@gmail.com>
2025-03-27 23:02:11 +0300

    Initialized

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,9 +1,9 @@
-import { ImSpinner2 } from 'react-icons/im';
+import { ImSpinner2 } from "react-icons/im";
 
 const Spinner = () => {
   return (
     <div>
-      <ImSpinner2 className='text-[24px] animate-spin mx-auto' />
+      <ImSpinner2 className="text-[24px] animate-spin mx-auto" />
     </div>
   );
 };
