bdc30c9316537e976c470950e4501b3bb78828f4
ashraf <sadarsign@gmail.com>
2025-03-27 23:01:51 +0300

    Initialized

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,29 +1,56 @@
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { RootState } from '../redux/store'
-import Profile from './profile'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { RootState } from "../redux/store";
+import Profile from "./profile";
 
 const Header = () => {
+  const loginState = useSelector((state: RootState) => state.loginSlice);
 
-    const loginState = useSelector((state: RootState) => state.loginSlice)
-
-
-    return (
-        <div className='flex items-center justify-around p-3'>
-            <div className="text-xl font-bold logo ">
-                <h1>BILKHAYR GYM</h1>
-            </div>
-            {loginState.data.isSuccess ? <div className='flex items-center gap-3'>
-                <Profile />
-            </div> : <div className="flex gap-4 menus">
-                <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/"}>HomePage</Link>
-                <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/login"}>Login</Link>
-                <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/auth/register"}>Register</Link>
-                <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/dashboard"}>Dashboard</Link>
-                <Link className='p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700' to={"/members"}>members</Link>
-            </div>}
+  return (
+    <div className="flex items-center justify-around p-3">
+      <div className="text-xl font-bold logo ">
+        <h1>BILKHAYR GYM</h1>
+      </div>
+      {loginState.data.isSuccess ? (
+        <div className="flex items-center gap-3">
+          <Profile />
         </div>
-    )
-}
+      ) : (
+        <div className="flex gap-4 menus">
+          <Link
+            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
+            to={"/"}
+          >
+            HomePage
+          </Link>
+          <Link
+            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
+            to={"/auth/login"}
+          >
+            Login
+          </Link>
+          <Link
+            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
+            to={"/auth/register"}
+          >
+            Register
+          </Link>
+          <Link
+            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
+            to={"/dashboard"}
+          >
+            Dashboard
+          </Link>
+          <Link
+            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
+            to={"/members"}
+          >
+            members
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
