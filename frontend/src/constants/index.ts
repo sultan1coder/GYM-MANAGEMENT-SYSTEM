1bea4c7aa5cd7036ad7f55ad48b924f9556b5809
ashraf <sadarsign@gmail.com>
2025-03-27 23:01:38 +0300

    Initialized

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,2 +1,3 @@
-export const DEFAULT_ERROR_MESSAGE = "Something went wrong, please try again later"
-export const BASE_API_URL = "http://localhost:4000/api"
+export const DEFAULT_ERROR_MESSAGE =
+  "Something went wrong, please try again later";
+export const BASE_API_URL = "http://localhost:4000/api";
