569c78cefb220e0b48012a6c619cfc4887efa713
ashraf <sadarsign@gmail.com>
2025-03-27 23:00:37 +0300

    Initialized

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,22 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const MainRouter = () => {
-    return (
-        <div className='flex flex-col h-screen'>
-            <div className="flex-1 header">
-                <Header />
-            </div>
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 header">
+        <Header />
+      </div>
 
-            <div className="content">
-                <Outlet />
-            </div>
+      <div className="content">
+        <Outlet />
+      </div>
 
-            <div className="footer bg-[#282c34] text-[#ffffff] mt-5 text-center py-6">
-                <footer />
-            </div>
-        </div>
-    )
-}
+      <div className="footer bg-[#282c34] text-[#ffffff] mt-5 text-center py-6">
+        <footer />
+      </div>
+    </div>
+  );
+};
 
 export default MainRouter;
