30c80805a3a13abb6b5346b38d3d7d1271c96e11
ashraf <sadarsign@gmail.com>
2025-04-20 20:13:35 +0300

    Initialized

--- frontend/src/redux/slices/auth/registerSlice.ts
+++ frontend/src/redux/slices/auth/registerSlice.ts
@@ -4,48 +4,53 @@ import { BASE_API_URL, DEFAULT_ERROR_MESSAGE } from "../../../constants";
 import { IRegisterBody, IRegisterResponse } from "../../../types/register";
 
 const initialState = {
-    loading: false,
-    data: {} as IRegisterResponse,
-    error: ""
-}
+  loading: false,
+  data: {} as IRegisterResponse,
+  error: "",
+};
 
-export const registerFn = createAsyncThunk("users/register", async (data: IRegisterBody, { rejectWithValue }) => {
+export const registerFn = createAsyncThunk(
+  "users/register",
+  async (data: IRegisterBody, { rejectWithValue }) => {
     try {
-        const response = await axios.post(`${BASE_API_URL}/users/register`, data);
-        return response.data;
+      const response = await axios.post(`${BASE_API_URL}/users/register`, data);
+      return response.data;
     } catch (error) {
-        if (error instanceof AxiosError) {
-            return rejectWithValue(error.response?.data.messaga || DEFAULT_ERROR_MESSAGE);
-        }
+      if (error instanceof AxiosError) {
+        return rejectWithValue(
+          error.response?.data.messaga || DEFAULT_ERROR_MESSAGE
+        );
+      }
 
-        return rejectWithValue(DEFAULT_ERROR_MESSAGE);
+      return rejectWithValue(DEFAULT_ERROR_MESSAGE);
     }
-})
+  }
+);
 
 export const registerSlice = createSlice({
-    name: "login slice",
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        //Pending..
-        builder.addCase(registerFn.pending, (state) => {
-            state.loading = true;
-            state.error = "";
-            state.data = {} as IRegisterResponse;
-        });
+  name: "login slice",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    //Pending..
+    builder.addCase(registerFn.pending, (state) => {
+      state.loading = true;
+      state.error = "";
+      state.data = {} as IRegisterResponse;
+    });
 
-        //Fulfilled
-        builder.addCase(registerFn.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = "";
-            state.data = action.payload;
-        });
+    //Fulfilled
+    builder.addCase(registerFn.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = "";
+      state.data = action.payload;
+    });
 
-        //Rejected
-        builder.addCase(registerFn.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload as string;
-            state.data = {} as IRegisterResponse;
-        });
-    }
+    //Rejected
+    builder.addCase(registerFn.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload as string;
+      state.data = {} as IRegisterResponse;
+    });
+  },
 });
