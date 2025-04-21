6a629cd69f57fabcd1b45f67cb297b74921a4ca2
ashraf <sadarsign@gmail.com>
2025-04-21 19:31:37 +0300

    Initialized

--- backend/src/controllers/members.controller.ts
+++ backend/src/controllers/members.controller.ts
@@ -4,193 +4,205 @@ import { MemberShipType, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 interface ICreateMembersPayload {
-    name: string,
-    email: string,
-    phone_number: string,
-    password: string,
-    confirmPassword: string,
-    age: number,
-    membershiptype: MemberShipType,
+  name: string;
+  email: string;
+  phone_number: string;
+  password: string;
+  confirmPassword: string;
+  age: number;
+  membershiptype: MemberShipType;
 }
 
 interface IUpdateMember {
-    member_id: string,
-    name: string,
-    email: string,
-    age: number,
-    membershiptype: MemberShipType,
+  member_id: string;
+  name: string;
+  email: string;
+  age: number;
+  membershiptype: MemberShipType;
 }
 
 export const getAllMembers = async (req: Request, res: Response) => {
-    try {
-        const members = await prisma.member.findMany();
-        res.status(200).json({
-            message: "Successfully fetched all members",
-            members
-        })
-    } catch (error) {
-        res.status(500).json({
-            isSuccess: false,
-            message: defaultErrorMessage
-        });
-    }
-}
-
+  try {
+    const members = await prisma.member.findMany();
+    res.status(200).json({
+      message: "Successfully fetched all members",
+      members,
+    });
+  } catch (error) {
+    res.status(500).json({
+      isSuccess: false,
+      message: defaultErrorMessage,
+    });
+  }
+};
 
 export const getSingleMember = async (req: Request, res: Response) => {
-    try {
-        const memberId = req.params.id;
-        const member = await prisma.member.findFirst({
-            where: {
-                id: memberId,
-            }
-        });
+  try {
+    const memberId = req.params.id;
+    const member = await prisma.member.findFirst({
+      where: {
+        id: memberId,
+      },
+    });
 
-        if (!member) {
-            res.status(404).json({
-                isSuccess: false,
-                message: "Member is not found!"
-            })
-            return;
-        }
-
-        res.status(200).json({
-            isSuccess: true,
-            message: "Successfully fetched a member",
-            member
-        })
-        return;
-    } catch (error) {
-        res.status(500).json({
-            isSuccess: false,
-            message: defaultErrorMessage,
-            error: JSON.stringify(error)
-        });
+    if (!member) {
+      res.status(404).json({
+        isSuccess: false,
+        message: "Member is not found!",
+      });
+      return;
     }
-    return;
-}
 
+    res.status(200).json({
+      isSuccess: true,
+      message: "Successfully fetched a member",
+      member,
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({
+      isSuccess: false,
+      message: defaultErrorMessage,
+      error: JSON.stringify(error),
+    });
+  }
+  return;
+};
 
 export const createMember = async (req: Request, res: Response) => {
-    try {
-        const { name, email, age, phone_number, password, confirmPassword, membershiptype } = req.body as ICreateMembersPayload;
+  try {
+    const {
+      name,
+      email,
+      age,
+      phone_number,
+      password,
+      confirmPassword,
+      membershiptype,
+    } = req.body as ICreateMembersPayload;
 
-        if (!name || !email || !age || phone_number||password||confirmPassword ||!membershiptype) {
-            res.status(400).json({
-                isSuccess: false,
-                message: "Validation error"
-            });
-            return;
-        }
-        const newMember = await prisma.member.create({
-            data: {
-                name,
-                email,
-                age,
-                phone_number,
-                password,
-                confirmPassword,
-                membershiptype,
-            }
-        });
-
-        res.status(200).json({
-            isSuccess: true,
-            message: "Member successfully created",
-            newMember
-        });
-        return;
-    } catch (error) {
-        console.log("Error: " + error)
-        res.status(500).json({
-            isSuccess: false,
-            message: "Server error!"
-        });
+    if (
+      !name ||
+      !email ||
+      !age ||
+      !phone_number ||
+      !password ||
+      !confirmPassword ||
+      !membershiptype
+    ) {
+      res.status(400).json({
+        isSuccess: false,
+        message: "Validation error",
+      });
+      return;
     }
-}
+    const newMember = await prisma.member.create({
+      data: {
+        name,
+        email,
+        age,
+        phone_number,
+        password,
+        confirmPassword,
+        membershiptype,
+      },
+    });
 
+    res.status(200).json({
+      isSuccess: true,
+      message: "Member successfully created",
+      newMember,
+    });
+    return;
+  } catch (error) {
+    console.log("Error: " + error);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Server error!",
+    });
+  }
+};
 
 export const updateMember = async (req: Request, res: Response) => {
-    try {
-        const { member_id, name, email, age, membershiptype } = req.body as IUpdateMember;
-        const member = await prisma.member.findFirst({
-            where: {
-                id: member_id,
-            }
-        });
+  try {
+    const { member_id, name, email, age, membershiptype } =
+      req.body as IUpdateMember;
+    const member = await prisma.member.findFirst({
+      where: {
+        id: member_id,
+      },
+    });
 
-        if (!member) {
-            res.status(404).json({
-                isSuccess: false,
-                message: "Member not found!"
-            });
-            return;
-        }
-
-        const updateMember = await prisma.member.update({
-            where: {
-                id: member.id,
-            },
-            data: {
-                name,
-                email,
-                age,
-                membershiptype
-            }
-        });
-
-        res.status(200).json({
-            isSuccess: true,
-            message: "Member successfully updated",
-            updateMember
-        });
-    } catch (error) {
-        console.log("Error: " + error)
-        res.status(500).json({
-            isSuccess: false,
-            message: "Server error!"
-        });
+    if (!member) {
+      res.status(404).json({
+        isSuccess: false,
+        message: "Member not found!",
+      });
+      return;
     }
-    return;
-}
 
+    const updateMember = await prisma.member.update({
+      where: {
+        id: member.id,
+      },
+      data: {
+        name,
+        email,
+        age,
+        membershiptype,
+      },
+    });
+
+    res.status(200).json({
+      isSuccess: true,
+      message: "Member successfully updated",
+      updateMember,
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Server error!",
+    });
+  }
+  return;
+};
 
 export const deleteMember = async (req: Request, res: Response) => {
-    try {
-        const memberId = req.params.id;
-        const member = await prisma.member.findFirst({
-            where: {
-                id: memberId
-            }
-        });
+  try {
+    const memberId = req.params.id;
+    const member = await prisma.member.findFirst({
+      where: {
+        id: memberId,
+      },
+    });
 
-        if (!member) {
-            res.status(404).json({
-                isSuccess: false,
-                message: "Member not found!"
-            });
+    if (!member) {
+      res.status(404).json({
+        isSuccess: false,
+        message: "Member not found!",
+      });
 
-            return;
-        }
-
-        const deleteMember = await prisma.member.delete({
-            where: {
-                id: member.id
-            }
-        });
-
-        res.status(200).json({
-            isSuccess: true,
-            message: "Member successfully deleted!",
-            deleteMember
-        });
-    } catch (error) {
-        console.log("Error: " + error)
-        res.status(500).json({
-            isSuccess: false,
-            message: "Server error!"
-        })
+      return;
     }
-}
+
+    const deleteMember = await prisma.member.delete({
+      where: {
+        id: member.id,
+      },
+    });
+
+    res.status(200).json({
+      isSuccess: true,
+      message: "Member successfully deleted!",
+      deleteMember,
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Server error!",
+    });
+  }
+};
