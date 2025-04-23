4a611d1ed08fa28110d88a1ac911ba0e35adf7f7
ashraf <sadarsign@gmail.com>
2025-04-23 21:34:40 +0300

    Initialized

--- backend/src/controllers/members.controller.ts
+++ backend/src/controllers/members.controller.ts
@@ -71,100 +71,107 @@ export const getSingleMember = async (req: Request, res: Response) => {
   return;
 };
 
-
 //Register Member
 export const registerMember = async (req: Request, res: Response) => {
-    try {
-        const { name, email, password, confirmPassword, phone_number, age, membershiptype} = req.body;
+  try {
+    const {
+      name,
+      email,
+      password,
+      confirmPassword,
+      phone_number,
+      age,
+      membershiptype,
+    } = req.body;
 
-        // Check if password and confirm password match
-        if (password !== confirmPassword) {
-            res.status(400).json({
-                isSuccess: false,
-                message: "Password must match"
-            });
-            return;
-        }
-        const existingMember = await prisma.member.findUnique({
-            where: {
-                email: email
-            }
-        });
-
-        // CHECK IF THE USER IS EXISTING
-        if (existingMember) {
-            res.status(400).json({
-                message: "Member already exists!",
-            });
-            return;
-        }
-
-        // HASH THE PASSWORD
-        const hashedPassword = await hashPassword(password);
-
-        // CREATE THE USER
-        const newMember = await prisma.member.create({
-            data: {
-                name,
-                email,
-                phone_number,
-                age,
-                membershiptype,
-                password: hashedPassword,
-                confirmPassword: hashedPassword,
-            }
-        });
-
-        res.status(201).json({
-            isSuccess: true,
-            message: "User registered successfully",
-            newMember
-        });
-    } catch (error) {
-        res.status(500).json({
-            messaga: "Something went wrong"
-        });
+    // Check if password and confirm password match
+    if (password !== confirmPassword) {
+      res.status(400).json({
+        isSuccess: false,
+        message: "Password must match",
+      });
+      return;
     }
-}
+    const existingMember = await prisma.member.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    // CHECK IF THE USER IS EXISTING
+    if (existingMember) {
+      res.status(400).json({
+        message: "Member already exists!",
+      });
+      return;
+    }
+
+    // HASH THE PASSWORD
+    const hashedPassword = await hashPassword(password);
+
+    // CREATE THE USER
+    const newMember = await prisma.member.create({
+      data: {
+        name,
+        email,
+        phone_number,
+        age,
+        membershiptype,
+        password: hashedPassword,
+        confirmPassword: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      isSuccess: true,
+      message: "User registered successfully",
+      newMember,
+    });
+  } catch (error) {
+    res.status(500).json({
+      messaga: "Something went wrong",
+    });
+  }
+};
 
 //Login Member
 export const loginMember = async (req: Request, res: Response) => {
-    try {
-        const { email, password } = req.body;
-        const member = await prisma.member.findUnique({
-            where: {
-                email: email
-            }
-        });
+  try {
+    const { email, password } = req.body;
+    const member = await prisma.member.findUnique({
+      where: {
+        email: email,
+      },
+    });
 
-        if (!member) {
-            res.status(401).json({
-                message: "incorrect email or password"
-            });
-            return;
-        }
-
-        const isMatch = await comparePassword(password, member.password);
-
-        if (!isMatch) {
-            res.status(400).json({
-                message: "Incorrect email or password"
-            });
-            return;
-        }
-        // Generate token
-        const token = generateToken(member.age);
-        res.status(200).json({
-            isSuccess: true,
-            member,
-            token
-        })
-    } catch (error) {
-        res.status(500).json({
-            messaga: "Something went wrong"
-        });
+    if (!member) {
+      res.status(401).json({
+        message: "incorrect email or password",
+      });
+      return;
     }
-}
+
+    const isMatch = await comparePassword(password, member.password);
+
+    if (!isMatch) {
+      res.status(400).json({
+        message: "Incorrect email or password",
+      });
+      return;
+    }
+    // Generate token
+    const token = generateToken(member.age);
+    res.status(200).json({
+      isSuccess: true,
+      member,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      messaga: "Something went wrong",
+    });
+  }
+};
 
 export const updateMember = async (req: Request, res: Response) => {
   try {
