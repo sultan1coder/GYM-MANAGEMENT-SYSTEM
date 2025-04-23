7749b4c88adda8d10c0280eb1b8c47c9094980d1
ashraf <sadarsign@gmail.com>
2025-04-23 21:29:29 +0300

    Initialized

--- backend/src/routes/members.route.ts
+++ backend/src/routes/members.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { deleteMember, getAllMembers, getSingleMember, registerMember, updateMember, } from "../controllers/members.controller";
+import { deleteMember, getAllMembers, loginMember, registerMember, updateMember, } from "../controllers/members.controller";
 import { subscribeMember, unsubscribeMember } from "../controllers/subscription.controller";
 import { adminRoute } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/list", getAllMembers);
-router.get("/single/:id", getSingleMember);
-router.post("/create",  registerMember);
+router.get("/login", loginMember);
+router.post("/register",  registerMember);
 router.put("/update/:id",  updateMember);
 router.delete("/delete/:id",  deleteMember);
 router.post("/:id/subscribe",  subscribeMember);
