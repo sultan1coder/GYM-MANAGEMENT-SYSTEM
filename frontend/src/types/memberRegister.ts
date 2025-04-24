8063f4d8c162fc5665e4144cddb2089b4c9c5b37
ashraf <sadarsign@gmail.com>
2025-04-24 23:32:33 +0300

    Initialized

--- frontend/src/types/memberRegister.ts
+++ frontend/src/types/memberRegister.ts
@@ -1,28 +1,19 @@
-export interface IloginResponseMember {
+export interface IRegisterMemberResponse {
   isSuccess: boolean;
-  message: string;
+  message:   string;
   newMember: NewMember;
 }
 
 export interface NewMember {
-  id: string;
-  name: string;
-  email: string;
-  phone_number: string;
-  password: string;
+  id:              string;
+  name:            string;
+  email:           string;
+  phone_number:    string;
+  password:        string;
   confirmPassword: string;
-  age: number;
-  membershiptype: string;
-  createdAt: Date;
-  updatedAt: Date;
+  age:             number;
+  membershiptype:  string;
+  createdAt:       Date;
+  updatedAt:       Date;
 }
 
-export interface IRegisterBodyMember {
-  name: string;
-  email: string;
-  age: number;
-  phone_number: string;
-  password: string;
-  confirmPassword: string;
-  membershiptype: string;
-}
