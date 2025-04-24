45c669156bce1e4f073cde150329cd9bdd6cad74
ashraf <sadarsign@gmail.com>
2025-04-24 23:36:26 +0300

    Initialized

---
+++ frontend/src/types/memberLogin.ts
@@ -0,0 +1,18 @@
+export interface ILoginMemberResponse {
+    isSuccess: boolean;
+    member:    Member;
+    token:     string;
+}
+
+export interface Member {
+    id:              string;
+    name:            string;
+    email:           string;
+    phone_number:    string;
+    password:        string;
+    confirmPassword: string;
+    age:             number;
+    membershiptype:  string;
+    createdAt:       Date;
+    updatedAt:       Date;
+}
