6b7e96900efb2f53625c26354214d4980ddf1004
ashraf <sadarsign@gmail.com>
2025-04-27 21:00:43 +0300

    Initialized

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { RootState } from "../redux/store";
 import Profile from "./profile";
-import MemberProfile from "./memberProfile";
 
 const Header = () => {
   const loginState = useSelector((state: RootState) => state.loginSlice);
@@ -15,51 +14,6 @@ const Header = () => {
       {loginState.data.isSuccess ? (
         <div className="flex items-center gap-3">
           <Profile />
-        </div>
-      ) : (
-        <div className="flex gap-4 menus">
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/"}
-          >
-            HomePage
-          </Link>
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/auth/login"}
-          >
-            Login
-          </Link>
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/auth/register"}
-          >
-            Register
-          </Link>
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/dashboard"}
-          >
-            Dashboard
-          </Link>
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/members/login"}
-          >
-          LoginMember
-          </Link>
-          <Link
-            className="p-3 text-white transition-all bg-gray-800 rounded-md hover:bg-gray-700"
-            to={"/members/register"}
-          >
-          RegisterMember
-          </Link>
-        </div>
-      )}
-
-{loginState.data.isSuccess ? (
-        <div className="flex items-center gap-3">
-          <MemberProfile />
         </div>
       ) : (
         <div className="flex gap-4 menus">
